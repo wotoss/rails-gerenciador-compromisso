be68e733d2c0f19d36aa2b1c26b76e17dc4724e2
wotoss <wotoss10@gmail.com>
2022-01-30 07:36:03 -0500

    modelando e configurando paginas e rotas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   def index
  #variavel de instância   
     @nome = "Woto"
- end
+  end
+  
+ #criando a action ou método para acesso da pagina sobre.html.erb
+   def sobre 
+   end
+
+ #criando a action ou método para acesso da pagina contato.html.erb  
+   def contato
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   #vou subistituir a primeira pagina padrão do rails por esta..
   #to para onde ele vai execultar esta ação => então teremos o nomedacontroller#nomedaaction
   root to: "pages#index"
+  
+  #usaremos o verbo get, rota sobre => sendo direcionado controller-pages#action-sobre
+  get 'sobre', to: 'pages#sobre'
+  #usaremos o verbo get, rota contato => sendo direcionado controller-pages#action-contato
+  get 'contato', to: 'pages#contato'
 end
